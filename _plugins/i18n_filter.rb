b91aa06dbcd7c180c1eadcc8e1c86cbc42e286a7
EclipsingLines <eclipsinglines@gmail.com>
2025-03-20 14:58:18 -0700

    new static solution

--- _plugins/i18n_filter.rb
+++ _plugins/i18n_filter.rb
@@ -3,12 +3,24 @@ module Jekyll
     def t(input)
       # Use the detected locale if available, otherwise fall back to the site's configured language
       lang = @context.registers[:page]['detected_locale'] || @context.registers[:site].config['detected_locale'] || @context.registers[:site].config['lang'] || 'en'
-      
+
       # Load the translations for the detected locale
-      translations = YAML.load_file("_data/translations/#{lang}.yml")
-      
-      # Return the translated text or the original text if no translation is found
-      translations[input] || input
+      begin
+        translations = YAML.load_file("_data/translations/#{lang}.yml")
+        # Return the translated text or the original text if no translation is found
+        translations[input] || input
+      rescue
+        input
+      end
+    end
+
+    def localized_content(input)
+      lang = @context.registers[:page]['detected_locale'] || @context.registers[:site].config['detected_locale'] || @context.registers[:site].config['lang'] || 'en'
+      if lang == 'es'
+        "#{input}-es"
+      else
+        input
+      end
     end
   end
 end
