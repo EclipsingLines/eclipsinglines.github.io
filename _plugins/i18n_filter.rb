07d4b0b32c7ccfefde9b4f79b38dc0075e66dd3e
EclipsingLines <eclipsinglines@gmail.com>
2025-03-20 13:17:53 -0700

    Revert "update theme to include lang switch"
    This reverts commit 80a256e05bd57cd1e96f20d93d9ca2a3d1f04776.

--- _plugins/i18n_filter.rb
+++ _plugins/i18n_filter.rb
@@ -1,13 +1,8 @@
 module Jekyll
   module I18nFilter
     def t(input)
-      # Use the detected locale if available, otherwise fall back to the site's configured language
-      lang = @context.registers[:page]['detected_locale'] || @context.registers[:site].config['detected_locale'] || @context.registers[:site].config['lang'] || 'en'
-      
-      # Load the translations for the detected locale
+      lang = @context.registers[:site].config['lang']
       translations = YAML.load_file("_data/translations/#{lang}.yml")
-      
-      # Return the translated text or the original text if no translation is found
       translations[input] || input
     end
   end
