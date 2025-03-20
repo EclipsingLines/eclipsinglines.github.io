80a256e05bd57cd1e96f20d93d9ca2a3d1f04776
EclipsingLines <eclipsinglines@gmail.com>
2025-03-20 12:20:45 -0700

    update theme to include lang switch

--- _plugins/i18n_filter.rb
+++ _plugins/i18n_filter.rb
@@ -1,8 +1,13 @@
 module Jekyll
   module I18nFilter
     def t(input)
-      lang = @context.registers[:site].config['lang']
+      # Use the detected locale if available, otherwise fall back to the site's configured language
+      lang = @context.registers[:page]['detected_locale'] || @context.registers[:site].config['detected_locale'] || @context.registers[:site].config['lang'] || 'en'
+      
+      # Load the translations for the detected locale
       translations = YAML.load_file("_data/translations/#{lang}.yml")
+      
+      # Return the translated text or the original text if no translation is found
       translations[input] || input
     end
   end
